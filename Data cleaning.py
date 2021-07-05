872a87d3d6a3003add618d7a028bd51c9bd9199b
Md. Shahrin Nakkhatra <64383902+Nakkhatra@users.noreply.github.com>
2021-07-06 03:18:38 +0600

    Update Data cleaning.py

--- Data cleaning.py
+++ Data cleaning.py
@@ -8,38 +8,14 @@ Created on Mon Jul  5 05:12:32 2021
 import pandas as pd 
 import re
 
-df= pd.read_csv(r"E:\Machine Learning\Webscraping\RoadAccidentsTheDailyObserver_regex.csv")
+df= pd.read_csv(r"E:\Machine Learning\Webscraping\RoadAccidentsTheDailyObserver_encoding_corrected.csv") #This is where the .csv file was located in my local machine,
+                                                                                                         #Drive link: https://docs.google.com/spreadsheets/d/1Esb-1DOV4PtajCx582nt1Z_F_-OwbwD7aWF-c165RgY/edit#gid=1705484839
 
-
-
-#df["bool"]= [(df["titles"][i] in df["News"][i]) for i in range(df.shape[0])]
-#df["replaced"]=[(df["News"][i].replace(df["titles"][i],"")) for i in range(df.shape[0])]
-
-# =============================================================================
-# #Removing the headline/title from the newsbody first, because of the improper punctuation and lack of whitespace
-# 
-# def getit(row):
-#  try:
-#   return row.News.replace(row.titles,"")
-#  except:
-#   return row.News # in case row.get("titles") return non-string
-# 
-# df["replaced"] = df.apply(getit , axis = 1)
-# 
-# df= df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-# 
-# 
-# #Adding the headline to the news body with a colon................
-# df["cleaned"]= df.titles+": "+df.replaced
-# =============================================================================
-
-#First, let's strip off the beginning and ending whitespaces from the whole
-#dataframe if the entries are string===========================================
+#First, let's strip off the beginning and ending whitespaces from the whole dataframe if the entries are string===========================================
 
 df= df.applymap(lambda x: x.strip() if isinstance(x, str) else x)   #.apply() is for a single pd series while .applymap() applies for the whole dataframe
 
-#Now, let's use apply method to replace the headlines that are already on the Newsbody
-#with proper punctuation ": " and those news which do not have title in the body, add
+#Now, let's use apply method to replace the headlines that are already on the Newsbody with proper punctuation ": " and those news which do not have title in the body, add
 #the title to the body for those with a ": "
 
 def addheadlines(row):
